5aef08c2537eceb128c91516383576c9386948f1
Daen Rebel <daenrebel035@gmail.com>
2017-11-02 18:05:39 +0100

    Changes

--- deployment/leaf.bash
+++ deployment/leaf.bash
@@ -12,7 +12,7 @@ sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git python-virtualenv python3-virtualenv curl ntp build-essential screen cmake pkg-config libboost-all-dev libevent-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev libgtest-dev libzmq3-dev
 cd ~
-git clone https://github.com/Snipa22/nodejs-pool.git  # Change this depending on how the deployment goes.
+git clone https://github.com/BlackBinary/nodejs-pool.git  # Change this depending on how the deployment goes.
 cd /usr/src/gtest
 sudo cmake .
 sudo make
@@ -23,7 +23,7 @@ cd /usr/local/src
 sudo git clone https://github.com/electroneum/electroneum.git
 cd electroneum
 sudo git checkout v0.11.0.0
-curl https://raw.githubusercontent.com/Snipa22/nodejs-pool/master/deployment/electroneum_daemon.patch | sudo git apply -v
+curl https://raw.githubusercontent.com/BlackBinary/nodejs-pool/master/deployment/electroneum_daemon.patch | sudo git apply -v
 sudo make -j$(nproc)
 sudo cp ~/nodejs-pool/deployment/electroneum.service /lib/systemd/system/
 sudo useradd -m electroneumdaemon -d /home/electroneumdaemon
--- deployment/upgrade_electroneum.bash
+++ deployment/upgrade_electroneum.bash
@@ -7,7 +7,7 @@ sudo git checkout .
 sudo git checkout master
 sudo git pull
 sudo git checkout origin/release-v0.11.0.0
-curl -L https://raw.githubusercontent.com/Snipa22/nodejs-pool/master/deployment/electroneum_daemon.patch | sudo git apply -v
+curl -L https://raw.githubusercontent.com/BlackBinary/nodejs-pool/master/deployment/electroneum_daemon.patch | sudo git apply -v
 sudo rm -rf build
 sudo make -j$(nproc)
 echo "Done building the new Electroneum daemon!  Please go ahead and reboot electroneum with: sudo systemctl restart electroneum as soon as the pool source is updated!"
